463a135af5b0de338d8cfd5bb07aa410c4db619f
namcartoon06 <hoangnam.ams06@gmail.com>
2026-02-20 17:56:59 -0600

    fixed background

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,20 @@
-import backgroundImage from "../../2d-assets/background/background3.png";
+import backgroundImage from "../../2d-assets/background/Background3.png";
 import sprite from "../../2d-assets/sprite/walk2.png";
 import "./App.css";
 
 function App() {
   return (
     <>
+      <div className="gallery">
+        <img src={backgroundImage} className="background"></img>
+      </div>
       <div>
         <img src={sprite} className="sprite"></img>
+      </div>
+      <div className="button-container">
         <button className="button">+</button>
+        <button className="button">Save</button>
+        <button className="button">Load</button>
       </div>
     </>
   );
