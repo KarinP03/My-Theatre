f1c196dad082e73fa79f4e2913c8171a7a0faf16
KarinP03 <k.plavin03@gmail.com>
2026-02-20 17:04:43 -0600

    Fix background image size attempt

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,25 @@
-import { useState } from 'react'
-import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import backgroundImage from '../../2d-assets/background/background3.png'
+import { useState } from "react";
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import backgroundImage from "../../2d-assets/background/background3.png";
 
 function App() {
   const bgStyle: React.CSSProperties = {
     backgroundImage: `url(${backgroundImage})`,
-    height: '100vh',
-    width: '200vh',
-    backgroundPosition: 'center',
-    backgroundSize: 'cover',
-  }
+    height: "100vh",
+    width: "200vh",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+  };
 
   return (
     <>
-      <div>
-        <img src={backgroundImage} className="background">
-        </img>
+      <div className="gallery">
+        <img src={backgroundImage} className="background"></img>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
