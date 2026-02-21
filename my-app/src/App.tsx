ac9a25e1ee977d1cf731b206bb116b6fa9ab5105
namcartoon06 <hoangnam.ams06@gmail.com>
2026-02-21 00:14:54 -0600

    added frontend stuff

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,17 +1,26 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { useEffect } from "react";
 import backgroundImage from "../../2d-assets/background/Background3.png";
-import sprite from "../../2d-assets/sprite/walk2.png";
+import walkl1 from "../../2d-assets/sprite/LeftWalk1.png";
+import walkl2 from "../../2d-assets/sprite/LeftWalk2.png";
+import walkl3 from "../../2d-assets/sprite/LeftWalk3.png";
+import walkr1 from "../../2d-assets/sprite/RightWalk1.png";
+import walkr2 from "../../2d-assets/sprite/RightWalk2.png";
+import walkr3 from "../../2d-assets/sprite/RightWalk3.png";
+import { draw } from "./animation";
 import "./App.css";
-import AddEntry from "./addEntry";
 
 function App() {
+  useEffect(() => {
+    // React has finished rendering, so the element now exists in the DOM
+    draw(walkr1, walkr2, walkr3, walkl1, walkl2, walkl3);
+  }, []);
   return (
     <>
       <div className="gallery">
         <img src={backgroundImage} className="background"></img>
       </div>
-      <div>
-        <img src={sprite} className="sprite"></img>
+      <div className="sprite-container">
+        <canvas id="sprite"></canvas>
       </div>
       <div className="button-container">
         <button className="button">+</button>
