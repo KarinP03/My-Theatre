fd6c28972acb2201fa2845a8b2ca68494dac2727
KarinP03 <k.plavin03@gmail.com>
2026-02-20 19:25:23 -0600

    Added page change attempt to formatted main page

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,22 +1,32 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import backgroundImage from "../../2d-assets/background/Background3.png";
 import sprite from "../../2d-assets/sprite/walk2.png";
 import "./App.css";
+import addEntry from "./addEntry";
 
 function App() {
   return (
-    <>
-      <div className="gallery">
-        <img src={backgroundImage} className="background"></img>
-      </div>
-      <div>
-        <img src={sprite} className="sprite"></img>
-      </div>
-      <div className="button-container">
-        <button className="button">+</button>
-        <button className="button">Save</button>
-        <button className="button">Load</button>
-      </div>
-    </>
+    <BrowserRouter>
+      <>
+        <div className="gallery">
+          <img src={backgroundImage} className="background"></img>
+        </div>
+        <div>
+          <img src={sprite} className="sprite"></img>
+        </div>
+        <div className="button-container">
+          <button className="button">
+            <Link to="/addEntry">+</Link>
+          </button>
+          <button className="button">Save</button>
+          <button className="button">Load</button>
+        </div>
+      </>
+
+      <Routes>
+        <Route path="/addEntry" Component={addEntry}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
