50525f136b545ce35c59b9918458ce27c82058ad
Ramatjyot Singh <ramatjyot13.ca@gmail.com>
2026-02-20 23:55:36 -0600

    fix router issue

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,25 +1,26 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import backgroundImage from "../../2d-assets/background/Background3.png";
 import sprite from "../../2d-assets/sprite/walk2.png";
 import "./App.css";
-import AddEntry from "./addEntry";
 
 function App() {
-  return (
-    <>
-      <div className="gallery">
-        <img src={backgroundImage} className="background"></img>
-      </div>
-      <div>
-        <img src={sprite} className="sprite"></img>
-      </div>
-      <div className="button-container">
-        <button className="button">+</button>
-        <button className="button">Save</button>
-        <button className="button">Load</button>
-      </div>
-    </>
-  );
-}
+    return (
+      <>
+        <div className="gallery">
+          <img src={backgroundImage} className="background"></img>
+        </div>
+        <div>
+          <img src={sprite} className="sprite"></img>
+        </div>
+        <div className="button-container">
+          <Link to="/addEntry">
+            <button className="button">+</button>
+          </Link>
+          <button className="button">Save</button>
+          <button className="button">Load</button>
+        </div>
+      </>
+    );
+  }
 
 export default App;
