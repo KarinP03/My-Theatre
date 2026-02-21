3f1a5b2bd88e06acf9f3a776b0998fdbc2d70634
KarinP03 <k.plavin03@gmail.com>
2026-02-20 19:43:54 -0600

    Moved browser router configaration location

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,32 +2,23 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import backgroundImage from "../../2d-assets/background/Background3.png";
 import sprite from "../../2d-assets/sprite/walk2.png";
 import "./App.css";
-import addEntry from "./addEntry";
+import AddEntry from "./addEntry";
 
 function App() {
   return (
-    <BrowserRouter>
-      <>
-        <div className="gallery">
-          <img src={backgroundImage} className="background"></img>
-        </div>
-        <div>
-          <img src={sprite} className="sprite"></img>
-        </div>
-        <div className="button-container">
-          <Link to="/addEntry">
-            <button className="button">+</button>
-          </Link>
-
-          <button className="button">Save</button>
-          <button className="button">Load</button>
-        </div>
-      </>
-
-      <Routes>
-        <Route path="/addEntry" Component={addEntry}></Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <div className="gallery">
+        <img src={backgroundImage} className="background"></img>
+      </div>
+      <div>
+        <img src={sprite} className="sprite"></img>
+      </div>
+      <div className="button-container">
+        <button className="button">+</button>
+        <button className="button">Save</button>
+        <button className="button">Load</button>
+      </div>
+    </>
   );
 }
 
