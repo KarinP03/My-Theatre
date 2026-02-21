f815e000d45e6306b90356f13282973e7b9b6b52
KarinP03 <k.plavin03@gmail.com>
2026-02-21 11:04:54 -0600

    Added more fields to form

--- my-app/src/addEntry.tsx
+++ my-app/src/addEntry.tsx
@@ -8,6 +8,30 @@ function addEntry() {
           <label>Movie name: </label>
           <input type="text" name="title" id="title"></input>
         </div>
+        <div className="entry-attribute">
+          <label>Date acquired: </label>
+          <input type="text" name="acquired" id="acquired"></input>
+        </div>
+        <div className="entry-attribute">
+          <label>Format: </label>
+          <input type="text" name="format" id="format"></input>
+        </div>
+        <div className="entry-attribute">
+          <label>Audio quality: </label>
+          <input type="text" name="audioQuality" id="audioQuality"></input>
+        </div>
+        <div className="entry-attribute">
+          <label>Date last watched: </label>
+          <input type="text" name="lastWatched" id="lastWatched"></input>
+        </div>
+        <div className="entry-attribute">
+          <label>Rating: </label>
+          <input type="text" name="rating" id="rating"></input>
+        </div>
+        <div className="entry-attribute">
+          <label>Notes: </label>
+          <input type="text" name="notes" id="notes"></input>
+        </div>
       </form>
     </>
   );
