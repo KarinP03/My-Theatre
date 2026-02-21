c4ebe432cc16b127880e2ee5b7cdd0938ae67152
KarinP03 <k.plavin03@gmail.com>
2026-02-21 10:56:17 -0600

    Added movie title field

--- my-app/src/addEntry.tsx
+++ my-app/src/addEntry.tsx
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function addEntry() {
-  return <div> hello world </div>;
+  return (
+    <>
+      <form action="" method="post" className="entry-form">
+        <div className="entry-attribute">
+          <label>Movie name: </label>
+          <input type="text" name="title" id="title"></input>
+        </div>
+      </form>
+    </>
+  );
 }
 
 export default addEntry;
