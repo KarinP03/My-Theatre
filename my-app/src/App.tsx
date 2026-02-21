57532f8cdaee9eba36cbd60a4059668036522d32
KarinP03 <k.plavin03@gmail.com>
2026-02-21 09:58:17 -0600

    Added modal attempt to app.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,25 +2,46 @@ import { Link } from "react-router-dom";
 import backgroundImage from "../../2d-assets/background/Background3.png";
 import sprite from "../../2d-assets/sprite/walk2.png";
 import "./App.css";
+// import detailsModal from "./detailsModal.tsx";
+import { useState } from "react";
+import Modal from "react-bootstrap/Modal";
 
 function App() {
-    return (
-      <>
-        <div className="gallery">
-          <img src={backgroundImage} className="background"></img>
-        </div>
-        <div>
-          <img src={sprite} className="sprite"></img>
-        </div>
-        <div className="button-container">
-          <Link to="/addEntry">
-            <button className="button">+</button>
-          </Link>
-          <button className="button">Save</button>
-          <button className="button">Load</button>
-        </div>
-      </>
-    );
-  }
+  const [modalVisible, setModalVisible] = useState(false);
+  const showModal = () => setModalVisible(true);
+  const hideModal = () => setModalVisible(false);
+
+  return (
+    <>
+      <div className="gallery">
+        <img src={backgroundImage} className="background"></img>
+      </div>
+      <div>
+        <img src={sprite} className="sprite"></img>
+      </div>
+      <div className="button-container">
+        <Link to="/addEntry">
+          <button className="button">+</button>
+        </Link>
+        <button className="button" onClick={showModal}>
+          Save
+        </button>
+        <button className="button">Load</button>
+      </div>
+
+      <Modal show={modalVisible} onHide={hideModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Movie title here</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h3>Date acquired: </h3>
+          <h3>Format: </h3>
+          <h3>Audio quality: </h3>
+          <h3>Last watched: never</h3>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+}
 
 export default App;
