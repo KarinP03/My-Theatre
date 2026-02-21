3ea22ca1532e3e24be9364cca728c4e8afe188d9
KarinP03 <k.plavin03@gmail.com>
2026-02-21 12:28:49 -0600

    Updated modal contents

--- my-app/src/addEntry.tsx
+++ my-app/src/addEntry.tsx
@@ -9,35 +9,42 @@ function addEntry() {
   const hideModal = () => setModalVisible(false);
   return (
     <>
-      <Modal>
-        <form action="" method="post" className="entry-form">
-          <div className="entry-attribute">
-            <label>Date acquired: </label>
-            <input type="text" name="acquired" id="acquired"></input>
-          </div>
-          <div className="entry-attribute">
-            <label>Format: </label>
-            <input type="text" name="format" id="format"></input>
-          </div>
-          <div className="entry-attribute">
-            <label>Audio quality: </label>
-            <input type="text" name="audioQuality" id="audioQuality"></input>
-          </div>
-          <div className="entry-attribute">
-            <label>Date last watched: </label>
-            <input type="text" name="lastWatched" id="lastWatched"></input>
-          </div>
-          <div className="entry-attribute">
-            <label>Rating: </label>
-            <input type="text" name="rating" id="rating"></input>
-          </div>
-          <div className="entry-attribute">
-            <label>Notes: </label>
-            <input type="text" name="notes" id="notes"></input>
-          </div>{" "}
-          <button onClick={hideModal}>Cancel</button>
-          <button onClick={hideModal}>Submit</button>
-        </form>
+      <Modal show={modalVisible} onHide={hideModal}>
+        <Modal.Header>
+          <Modal.Title>Movie Entry Details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form action="" method="post" className="entry-form">
+            <div className="entry-attribute">
+              <label>Date acquired: </label>
+              <input type="text" name="acquired" id="acquired"></input>
+            </div>
+            <div className="entry-attribute">
+              <label>Format: </label>
+              <input type="text" name="format" id="format"></input>
+            </div>
+            <div className="entry-attribute">
+              <label>Audio quality: </label>
+              <input type="text" name="audioQuality" id="audioQuality"></input>
+            </div>
+            <div className="entry-attribute">
+              <label>Date last watched: </label>
+              <input type="text" name="lastWatched" id="lastWatched"></input>
+            </div>
+            <div className="entry-attribute">
+              <label>Rating: </label>
+              <input type="text" name="rating" id="rating"></input>
+            </div>
+            <div className="entry-attribute">
+              <label>Notes: </label>
+              <input type="text" name="notes" id="notes"></input>
+            </div>
+            <div className="buttons">
+              <button onClick={hideModal}>Cancel</button>
+              <button onClick={hideModal}>Submit</button>
+            </div>
+          </form>
+        </Modal.Body>
       </Modal>
     </>
   );
